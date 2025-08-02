fad9ce55c358127fee67910e95ced840524e4c74
ahmadhussantamim <aht2560@gmail.com>
2025-08-02 23:35:01 +0600

    Update Main.java

--- Voting_info1/src/Main.java
+++ Voting_info1/src/Main.java
@@ -1,14 +1,12 @@
-
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
+import java.awt.Color;
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.awt.Font;
 import javax.swing.JLabel;
-
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
@@ -48,4 +46,5 @@ public class Main {
             }
         });
 	}
+
 }
