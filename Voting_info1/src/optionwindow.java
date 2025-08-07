ccc9ac5e365a22e16beef192cedf2c8bce3537fd
ahmadhussantamim <aht2560@gmail.com>
2025-08-08 00:37:45 +0600

    Update optionwindow.java

--- Voting_info1/src/optionwindow.java
+++ Voting_info1/src/optionwindow.java
@@ -7,34 +7,49 @@ public class optionwindow {
         frame.setSize(500, 300);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        ImageIcon bIcon = new ImageIcon("images/option.jpg");
+        Image sc = bIcon.getImage().getScaledInstance(500, 300, Image.SCALE_SMOOTH);
+        ImageIcon backgroundIcon2 = new ImageIcon(sc);
+        
+        JLabel bLabel = new JLabel(backgroundIcon2);
+        bLabel.setBounds(0, 0, 500, 300);
+        frame.setContentPane(bLabel);
+        bLabel.setLayout(null);
 
         JLabel messageLabel = new JLabel(personName + ", you're NOT eligible to vote yet.");
-        messageLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        messageLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        messageLabel.setForeground(Color.BLACK);
         messageLabel.setBounds(40, 30, 450, 30);
         frame.add(messageLabel);
 
         JLabel questionLabel = new JLabel("Do you want to apply for NID registration?");
-        questionLabel.setFont(new Font("Arial", Font.BOLD, 16));
-        questionLabel.setBounds(40, 80, 420, 30);
+        questionLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        questionLabel.setForeground(Color.BLACK);
+        questionLabel.setBounds(40, 70, 420, 30);
         frame.add(questionLabel);
 
         JButton yesButton = new JButton("Yes");
-        yesButton.setFont(new Font("Arial", Font.BOLD, 16));
+        yesButton.setFont(new Font("Arial", Font.BOLD, 22));
         yesButton.setBounds(120, 150, 100, 40);
+        yesButton.setBackground(Color.GREEN);
+        yesButton.setForeground(Color.BLACK);
+        yesButton.setFocusPainted(false);
         frame.add(yesButton);
 
         JButton noButton = new JButton("No");
-        noButton.setFont(new Font("Arial", Font.BOLD, 16));
+        noButton.setFont(new Font("Arial", Font.BOLD, 22));
         noButton.setBounds(260, 150, 100, 40);
+        noButton.setBackground(Color.RED);
+        noButton.setForeground(Color.WHITE);
+        noButton.setFocusPainted(false);
         frame.add(noButton);
 
-        // Yes button action
         yesButton.addActionListener(e -> {
             new NIDRegistrationWindow(personName, 17);
             frame.dispose();
         });
 
-        // No button action
         noButton.addActionListener(e -> frame.dispose());
         
         frame.setLocationRelativeTo(null);
